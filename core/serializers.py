43e262cda7ab2986df1493791ce0549ea3ada8b1
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-04-14 12:24:35 +0500

    add login endpoint

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 
 
 USER_MODEL = get_user_model()
@@ -28,6 +29,24 @@ class RegistrationSerializer(serializers.ModelSerializer):
         validated_data['password'] = make_password(password)
         instance = super().create(validated_data)
         return instance
+
+    class Meta:
+        model = USER_MODEL
+        fields = '__all__'
+
+
+class LoginSerializers(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def create(self, validated_data):
+        user = authenticate(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        if not user:
+            raise AuthenticationFailed
+        return user
 
     class Meta:
         model = USER_MODEL
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from rest_framework import generics
+from rest_framework.response import Response
 
-from core.serializers import RegistrationSerializer
+from core.serializers import RegistrationSerializer, LoginSerializers
 
 USER_MODEL = get_user_model()
 
@@ -9,5 +10,17 @@ USER_MODEL = get_user_model()
 class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
     serializer_class = RegistrationSerializer
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializers
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
+
 
 
